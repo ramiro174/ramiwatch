9259c25ca156901d789b4e2275ec182a134015b5
17090077 <17090077>
2021-12-11 12:18:04 -0600

    Lectura de Sensor de Luz en Reloj

--- mobile/src/main/java/com/example/myapplication/MainActivity.kt
+++ mobile/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.ram2
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
--- wear/src/main/java/com/example/myapplication/MainActivity.kt
+++ wear/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.ram2
 
 import android.app.Activity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import com.example.ram2.databinding.ActivityMainBinding
+import android.widget.TextView
+import android.widget.Toast
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : Activity() {
+
+class MainActivity : Activity() , SensorEventListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private var mSensor: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +25,39 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
     }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        val lux = event.values[0]
+
+        findViewById<TextView>(R.id.luz_contenido).setText(lux.toString())
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mSensor?.also { light ->
+            sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    fun mandarNotificacion(desu : android.view.View)
+    {
+        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
